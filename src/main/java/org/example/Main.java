3836c8b211199aeb0ed457f8664dbdabf79aa5a7
shraddhayadav24 <slyadav240901@gmail.com>
2024-06-12 16:15:57 +0530

    File added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,13 @@
 package org.example;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
-   public static void firstname(String firstname){
-       Pattern pattern=Pattern.compile("[A-Z][a-z]{3,}");
-       Matcher matcher=pattern.matcher(firstname);
-       boolean ismatch=matcher.matches();
-       if (ismatch){
-           System.out.println("Valid input");
-       }else {
-           System.out.println("Invalid Input");
-       }
-   }
+
 
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter the name: ");
-        String firstname=scanner.nextLine();
-        firstname(firstname);
+
+        System.out.println("hello world ");
+
     }
 }
